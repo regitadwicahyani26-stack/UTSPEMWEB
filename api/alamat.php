659740abfc2dc2bcbfa029a7a6be406c37b099c4
Maria Teofani Evernita Sagala <regitadwicahyani_26@student.uns.ac.id>
2026-04-30 15:25:08 +0700

    up

--- api/alamat.php
+++ api/alamat.php
@@ -1,43 +1,43 @@
 <?php
-// api/alamat.php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
 
-$api_key = "c8909a4eb41f1f05b852f7170f919a6d";
-$url = "https://webapi.bps.go.id/v1/api/list/model/data/lang/ind/domain/0000/var/2221/th/126/key/{$api_key}";
+header("Content-Type: application/json");
+
+$url="https://webapi.bps.go.id/v1/api/view/domain/0000/model/statictable/lang/ind/id/950/key/c8909a4eb41f1f05b852f7170f919a6d";
 
 $curl = curl_init();
+
 curl_setopt_array($curl, [
-    CURLOPT_URL            => $url,
+    CURLOPT_URL => $url,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_SSL_VERIFYPEER => false,
-    CURLOPT_SSL_VERIFYHOST => false,
-    CURLOPT_TIMEOUT        => 30,
-    CURLOPT_USERAGENT      => 'Mozilla/5.0',
-    CURLOPT_FOLLOWLOCATION => true,
+
+    // INI PENTING !!!
+    CURLOPT_HTTPHEADER => [
+        "User-Agent: Mozilla/5.0",
+        "Accept: application/json"
+    ]
 ]);
 
 $response = curl_exec($curl);
-$err      = curl_error($curl);
+
+if(curl_errno($curl)){
+    echo json_encode([
+        "error"=>curl_error($curl)
+    ]);
+    exit;
+}
+
 $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
 curl_close($curl);
 
-if ($err) {
-    echo json_encode(["status" => "ERROR", "message" => "cURL Error: " . $err]);
+// cek kalau masih 403
+if($httpCode == 403){
+    echo json_encode([
+        "error"=>"403 Forbidden - API BPS menolak request"
+    ]);
     exit;
 }
 
-if ($httpCode !== 200) {
-    echo json_encode(["status" => "ERROR", "message" => "HTTP Error: " . $httpCode]);
-    exit;
-}
-
-$data = json_decode($response, true);
-
-if (json_last_error() !== JSON_ERROR_NONE) {
-    echo json_encode(["status" => "ERROR", "message" => "Response bukan JSON valid"]);
-    exit;
-}
-
-echo json_encode($data);
+echo $response;
 ?>
