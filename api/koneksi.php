e1592bb5f7014bd971f6168a7610c52bb65961f5
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-30 14:43:23 +0700

    up

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,7 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
+$port = 4000
+$user = "3ymotaaKzzAD8W5.root";
+$pass = "9M4ha2k6HvOhlgQY";
 $db   = "restoran"; // <-- Ini yang diubah (menyesuaikan nama di phpMyAdmin)
 
 $conn = mysqli_connect($host, $user, $pass, $db);
