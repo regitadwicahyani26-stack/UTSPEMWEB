8b53dc3a32471bfb73c910383837f2a897ce4329
Maria Teofani Evernita Sagala <regitadwicahyani_26@student.uns.ac.id>
2026-04-30 14:56:12 +0700

    up

--- api/koneksi.php
+++ api/koneksi.php
@@ -3,11 +3,27 @@ $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
 $port = 4000;
 $user = "3ymotaaKzzAD8W5.root";
 $pass = "9M4ha2k6HvOhlgQY";
-$db   = "restoran"; // <-- Ini yang diubah (menyesuaikan nama di phpMyAdmin)
+$db   = "restoran";
 
-$conn = mysqli_connect($host, $port, $user, $pass, $db);
+$koneksi = mysqli_init(); // ← bukan mysql_init()
 
-if (!$conn) {
+$ca_path = '/etc/ssl/certs/ca-certificates.crt';
+mysqli_ssl_set($koneksi, NULL, NULL, $ca_path, NULL, NULL);
+
+$real_connect = mysqli_real_connect(
+    $koneksi,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) { // ← bukan $conn, tapi $real_connect
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+$conn = $koneksi; // ← tambahkan ini agar $conn bisa dipakai di file lain
 ?>
